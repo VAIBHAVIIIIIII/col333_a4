bc8a69da74637bc106b57fbbb4fd9924103f986d
VAIBHAVIIIIIII <162910306+VAIBHAVIIIIIII@users.noreply.github.com>
2025-10-26 02:34:20 +0530

    Update compile.sh
    Updated compile script to use C++17 standard with higher optimization and warning flags.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-g++ -std=c++11 -O2 starter.cpp -o starter
+#!/bin/bash
+
+# Define the name of the main source file
+SOURCE_FILE="starter.cpp"
+
+# Define the output executable name
+EXECUTABLE_NAME="starter"
+
+# Compile the code using g++ with C++17 standard, optimization (-O3), 
+# and all warnings enabled (-Wall).
+g++ -std=c++17 -O3 -Wall $SOURCE_FILE -o $EXECUTABLE_NAME
+
 if [ $? -eq 0 ]; then
   echo "Compiled successfully: ./starter"
 else
