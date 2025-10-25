b0e6920b625d779247f53204a88ed10a35965dee
VAIBHAVIIIIIII <162910306+VAIBHAVIIIIIII@users.noreply.github.com>
2025-10-26 02:35:09 +0530

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-#!/bin/bash
 
-# Define the name of the main source file
+# main source file
 SOURCE_FILE="starter.cpp"
 
-# Define the output executable name
+# output executable name
 EXECUTABLE_NAME="starter"
 
-# Compile the code using g++ with C++17 standard, optimization (-O3), 
-# and all warnings enabled (-Wall).
+# Compile the code using g++ with C++17 standard, optimization (-O3), and all warnings enabled (-Wall).
 g++ -std=c++17 -O3 -Wall $SOURCE_FILE -o $EXECUTABLE_NAME
 
 if [ $? -eq 0 ]; then
