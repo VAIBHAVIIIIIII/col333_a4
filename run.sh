1396e225b6ac248bd8d68736ca0b6c7b316b5adf
root <root@DESKTOP-A7JBU6C.>
2025-10-27 06:22:36 +0000

    Deleted compile.sh and run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# $1 is expected to be the .bif file (e.g., hailfinder.bif)
-# $2 is expected to be the data file (e.g., records.dat)
-# Execute the compiled C++ program, passing the required two input files.
-echo "Execution starting..."
-./starter $1 $2
-echo "Execution complete. Check solved_hailfinder.bif for the learned network."
